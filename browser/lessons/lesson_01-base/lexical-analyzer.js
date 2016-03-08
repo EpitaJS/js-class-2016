e647554458a60e28b21fee34949135622e6c6d80
Cormery Antoine <antoine.cormery@epita.fr>
2016-03-08 09:38:52 +0100

    stem function

--- browser/lessons/lesson_01-base/lexical-analyzer.js
+++ browser/lessons/lesson_01-base/lexical-analyzer.js
@@ -1,81 +1,82 @@
 /** A simple lexical analyzer
- */
+*/
 
 /** tokenize a string = split it into words
- *
- * Note : space separated only for now
- *
- * @argument {String}
- * @return {Array}
- */
- export function tokenize(str) {
-     // Write code here so that it passes the tests
-     var tab = str.split(" ");
-     var i = 0;
-     while (i < tab.length) {
-         if (tab[i] == "") {
-             tab.splice(i, 1);
-         } else {
-             i += 1;
-         }
-     }
-     return tab;
- }
+*
+* Note : space separated only for now
+*
+* @argument {String}
+* @return {Array}
+*/
+export function tokenize(str) {
+    // Write code here so that it passes the tests
+    var tab = str.split(" ");
+    var i = 0;
+    while (i < tab.length) {
+        if (tab[i] == "") {
+            tab.splice(i, 1);
+        } else {
+            i += 1;
+        }
+    }
+    return tab;
+}
 
 /** stem a string = turn several variants into the same
- *
- * Note : for now, uniformize to lowercase only
- * @argument {String}
- * @return {String}
- */
+*
+* Note : for now, uniformize to lowercase only
+* @argument {String}
+* @return {String}
+*/
 export function stem(str) {
-  // Write code here so that it passes the tests
+    // Write code here so that it passes the tests
+    return str.toLowerCase();
 }
 
 /** parse a string into a list of stemmed token
- * i.e. combine the 2 functions above
- *
- * @argument {String}
- * @return {Array}
- */
+* i.e. combine the 2 functions above
+*
+* @argument {String}
+* @return {Array}
+*/
 export function parse(str) {
-  // Write code here so that it passes the tests
+    // Write code here so that it passes the tests
 }
 
 /** index a string into a hash {'token' : <frequency of appearance>}
- *
- * @argument {String}
- * @return {Object}
- */
+*
+* @argument {String}
+* @return {Object}
+*/
 export function index(str) {
-  // Write code here so that it passes the tests
+    // Write code here so that it passes the tests
 }
 
 
 /* Hints :
- *
- ******* Array *******
+*
+******* Array *******
 
- Array.push(<new item>)
+Array.push(<new item>)
 
- array.forEach(value => {
-   ...
- });
+array.forEach(value => {
+...
+});
 
- Array.map(value => {
-   return <newValue>;
- });
+Array.map(value => {
+return <newValue>;
+});
 
- Array.reduce((accumulator, value) => {
-   accumulator += value;
-   return accumulator;
- }, 0);
+Array.reduce((accumulator, value) => {
+accumulator += value;
+return accumulator;
+}, 0);
 
- ******* String *******
+******* String *******
 
- String.split(<separator>) -> Array
+String.split(<separator>) -> Array
 
- String.toLowerCase()
+String.toLowerCase()
 
- *
- */
+*
+*/
