dd42eaf267ede305f90e061cb4a313ebaff72897
Robin THONI <robin@rthoni.com>
2016-03-08 09:58:51 +0100

    lesson 3

--- browser/lessons/lesson_03-type_detection/boolean-converter.js
+++ browser/lessons/lesson_03-type_detection/boolean-converter.js
@@ -4,7 +4,16 @@
  * @return {Boolean}
  */
 export default function convertToBoolean(value) {
-  // TODO write the function so it passes the tests below !
+  if (typeof value === 'boolean') {
+    return value;
+  }
+  if (value instanceof Boolean) {
+    return value == true;
+  }
+  if (typeof value === 'number') {
+    return value == 1;
+  }
+  return value == "true";
 }
 
 
