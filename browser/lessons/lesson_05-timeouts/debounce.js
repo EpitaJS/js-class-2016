7d5e10e01903c098f96431396b4ac2af831c2189
Robin THONI <robin@rthoni.com>
2016-03-08 10:43:24 +0100

    lesson 5

--- browser/lessons/lesson_05-timeouts/debounce.js
+++ browser/lessons/lesson_05-timeouts/debounce.js
@@ -1,8 +1,17 @@
 
 export default function debounce(callback, waitMs) {
 
+  var id = null;
+
   return function() {
-    // ...
+    var args = arguments;
+    if (id) {
+      clearTimeout(id)
+    }
+    id = setTimeout(function()
+    {
+      callback.apply(window, args);
+    }, waitMs);
   };
 }
 
