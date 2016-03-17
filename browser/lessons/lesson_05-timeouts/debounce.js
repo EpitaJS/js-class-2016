b48638662b12815998318e6cff9dc82cbbd4748c
Pierre-Antoine 'ZHAJOR' Tible <antoinetible@gmail.com>
2016-03-17 19:17:18 +0100

    Exo 1,2,3,4,5,6

--- browser/lessons/lesson_05-timeouts/debounce.js
+++ browser/lessons/lesson_05-timeouts/debounce.js
@@ -1,8 +1,12 @@
 
 export default function debounce(callback, waitMs) {
 
+  var num = null;
+
   return function() {
-    // ...
+    var args = arguments;
+    clearTimeout(num);
+    num = setTimeout(function() { callback.apply(window, args);}, waitMs);
   };
 }
 
