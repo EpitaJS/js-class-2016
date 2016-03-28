2fa6665e340815a784df8b9cd4e1f4961b6fa6e1
betbed_t <thibaut.betbeder@epita.fr>
2016-03-28 02:30:31 +0200

    5 finished

--- browser/lessons/lesson_05-timeouts/debounce.js
+++ browser/lessons/lesson_05-timeouts/debounce.js
@@ -1,8 +1,16 @@
 
 export default function debounce(callback, waitMs) {
+ var timeoutHandle;
 
-  return function() {
-    setTimeout(callback,waitMs, "hello", 42);
+  return function(arg1, arg2) {
+    clearTimeout(timeoutHandle);
+     timeoutHandle = setTimeout(function(){
+       if(arg2 == null)
+       callback(arg1);
+       else
+        callback(arg1, arg2);
+
+    },waitMs);
   };
 }
 
